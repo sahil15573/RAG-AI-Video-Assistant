902b6423d23a420b67930ff5893450852c16986e
Sahil Kumar <sahilkr.15573@gmail.com>
2026-05-24 03:09:27 +0530

    Fix YouTube 403 permanently

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -30,7 +30,7 @@ def download_youtube_audio(url: str) -> str:
         # MOST IMPORTANT FIX
         "extractor_args": {
             "youtube": {
-                "player_client": ["android_creator"]
+                "player_client": ["tv_embedded", "android"]
             }
         },
 
