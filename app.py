682c95206d5478d5e92588ee7466ae6d061c30e2
Sahil Kumar <sahilkr.15573@gmail.com>
2026-05-24 01:03:27 +0530

    Add Streamlit secrets

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import time
-from dotenv import load_dotenv
+
 from utils.audio_processor import process_input
 from core.transcriber import transcribe_all
 from core.summarizer import summarize, generate_title
 from core.extractor import extract_action_items, extract_key_decisions, extract_questions
 from core.rag_engine import build_rag_chain, ask_question
 
-load_dotenv()
+MISTRAL_API_KEY = st.secrets["MISTRAL_API_KEY"]
+WHISPER_MODEL = st.secrets["WHISPER_MODEL"]
+SARVAM_API_KEY = st.secrets["SARVAM_API_KEY"]
+SARVAM_STT_MODEL = st.secrets["SARVAM_STT_MODEL"]
 
 # ─── Page Config ────────────────────────────────────────────────────────────────
 st.set_page_config(
